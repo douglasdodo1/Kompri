f89a31802568d1de0707128aa0de18b64c3a4548
Douglas Gemir <douglasgemir@gmail.com>
2025-07-01 00:32:24 -0300

    fix: relação entre compra e usuario

--- app/models/compra.rb
+++ app/models/compra.rb
@@ -1,10 +1,10 @@
 class Compra < ApplicationRecord
-  belongs_to :user, presence: true, foreign_key: true
-  belongs_to :instituicao, presence: true, foreign_key: true
+  belongs_to :usuario, foreign_key: :usuario_cpf, primary_key: :cpf, class_name: "Usuario"
+  belongs_to :instituicao
 
+  validates :usuario_cpf, presence: true
   validates :status, presence: true, inclusion: { in: %w[pendente concluida cancelada] }
-  validates :valor_total, presence: true, numericality: { greater_than: 0 }
+  validates :valor_total, presence: true, numericality: { greater_than: 0, message: :not_a_number }
   validates :valor_estimado, presence: true, numericality: { greater_than: 0 }
   validates :qtd_itens, presence: true, numericality: { greater_than: 0 }
-
 end
--- db/migrate/20250630183255_create_compras.rb
+++ db/migrate/20250630183255_create_compras.rb
@@ -1,8 +1,8 @@
 class CreateCompras < ActiveRecord::Migration[8.0]
   def change
     create_table :compras do |t|
-      t.references :user, null: false, foreign_key: true
-      t.string :status, null: false
+      t.string :usuario_cpf, null: false
+      t.string :status, null: false, default: "pendente"
       t.references :instituicao, null: false, foreign_key: true
       t.decimal :valor_total, null:false, precision: 10, scale: 2
       t.decimal :valor_estimado, null:false, precision: 10, scale: 2
@@ -10,5 +10,7 @@ class CreateCompras < ActiveRecord::Migration[8.0]
 
       t.timestamps
     end
+
+    add_foreign_key :compras, :usuarios, column: :usuario_cpf, primary_key: :cpf
   end
 end
