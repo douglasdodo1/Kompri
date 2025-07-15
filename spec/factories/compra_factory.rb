dc4b2cf6afd2a116776dd4bedfd233fd35acc4c5
Douglas Gemir <dcg@cin.ufpe.br>
2025-07-15 12:06:02 -0300

    feat: adicionar testes para a criação e validação de compras fix: ajustar a ação de set_compra no controller de compras

---
+++ spec/factories/compra_factory.rb
@@ -0,0 +1,50 @@
+FactoryBot.define do
+  factory :compra do
+    association(:usuario)
+    status { "pendente" }
+    instituicao { association(:instituicao) }
+    valor_total { 100.00 }
+    valor_estimado { 80.00 }
+    qtd_itens { 2 }
+
+    after(:build) do |compra|
+      compra.usuario_cpf ||= compra.usuario&.cpf
+    end
+
+    trait :usuario_cpf_vazio do
+      usuario_cpf { '' }
+    end
+
+    trait :status_vazio do
+      status { '' }
+    end
+
+    trait :status_invalido do
+      status { 'invalido' }
+    end
+
+    trait :valor_total_vazio do
+      valor_total { nil }
+    end
+
+    trait :valor_total_negativo do
+      valor_total { -10.00 }
+    end
+
+    trait :valor_estimado_vazio do
+      valor_estimado { nil }
+    end
+
+    trait :valor_estimado_negativo do
+      valor_estimado { -10.00 }
+    end
+
+    trait :qtd_itens_vazio do
+      qtd_itens { nil }
+    end
+
+    trait :qtd_itens_negativo do
+      qtd_itens { -1 }
+    end
+  end
+end
